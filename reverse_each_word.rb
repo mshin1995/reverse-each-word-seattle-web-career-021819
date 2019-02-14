363ca5f832c7aa8e68c2cb26fc26aef168825071
Matthew Shin <mshin1995@gmail.com>
2019-02-14 03:18:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   array1 = sentence.split
   array2 = []
-  array 2 = array1.collect do |words|
+  array2 = array1.collect do |words|
     words.reverse
   end
-  arra
+  array2.join(" ")
 end
